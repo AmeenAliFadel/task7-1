cd6e36c3b9adcb51d2766615961afbe92c4141da
AmeenFadel <ameenfadel820@gmail.com>
2024-11-12 22:48:48 +0300

    first commit

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,7 @@
+import './Map.css'
+
+export default function Map() {
+    return (
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d103885.1938642458!2d35.87441736741415!3d35.54351812264928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1526ac2d61d4607d%3A0x8e325bf8a14195de!2z2KfZhNmE2KfYsNmC2YrYqdiMINiz2YjYsdmK2Kc!5e0!3m2!1sar!2s!4v1731354278466!5m2!1sar!2s"  loading="lazy" referrerpolicy="no-referrer-when-downgrade" />
+    )
+}
